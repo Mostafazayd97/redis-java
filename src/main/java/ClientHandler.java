5ab2492c11ddb68f76fc6193986d9de1bdb6c1dc
MostafaZayed <mostafa.zayed@hypercell.net>
2025-01-11 16:59:55 +0200

    add set and get

--- src/main/java/ClientHandler.java
+++ src/main/java/ClientHandler.java
@@ -5,57 +5,59 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 import java.net.Socket;
+import java.util.HashMap;
 
 public class ClientHandler implements Runnable {
 
     private final Socket socket;
+    public static HashMap<String, String> map = new HashMap<>();
 
     public ClientHandler(Socket socket) { this.socket = socket; }
 
     @Override
 
     public void run() {
-
         try (BufferedReader input = new BufferedReader(
-
                 new InputStreamReader(socket.getInputStream()))) {
-
             while (true) {
-
                 String request = input.readLine();
-
                 if (request == null) {
-
                     continue;
-
                 }
-
                 if ("PING".equals(request)) {
-
                     socket.getOutputStream().write("+PONG\r\n".getBytes());
-
                 } else if ("ECHO".equalsIgnoreCase(request)) {
-
                     input.readLine();
-
                     String message = input.readLine();
-
                     socket.getOutputStream().write(
-
-                            String.format("$%d\r\n%s\r\n", message.length(), message)
-
+                            String.format("\r\n%s\r\n", message)
                                     .getBytes());
+                }
+            else if (request.startsWith("SET")) {
+                   String[] parts = request.split(" ");
 
+                    String key = parts[1];
+                    String value =parts[2];
+                    map.put(key, value);
+                    System.out.println(map.get(key)+ " sssssssssssssssss");
+                    socket.getOutputStream().write("+OK\r\n".getBytes());
                 }
 
+            else if (request.startsWith("GET")) {
+
+                    String key = request.split(" ")[1];
+                    String value = map.get(key);
+                    if (value == null) {
+                        socket.getOutputStream().write("$-1\r\n".getBytes());
+                    } else {
+                        socket.getOutputStream().write(
+                                String.format("n%s\r\n", value)
+                                        .getBytes());
+                    }
+                }
             }
-
         } catch (IOException e) {
-
             System.out.println("IOException: " + e.getMessage());
-
         }
-
     }
-
 }
